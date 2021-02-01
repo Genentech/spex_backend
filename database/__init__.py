852d489109f0ffd2a952efea41269e10898b4a75
Artem Zubkov <zubkov.artem@gene.com>
2021-02-01 17:38:52 +0700

    refactoring database

--- database/__init__.py
+++ database/__init__.py
@@ -1,6 +1,9 @@
+from os import getenv
 from database.model import ArangoDB
-from config import ARANGODB_DATABASE_URL, ARANGODB_DATABASE_NAME, ARANGODB_USERNAME, ARANGODB_PASSWORD
 
-database: ArangoDB = ArangoDB(
-    hosts=ARANGODB_DATABASE_URL, database=ARANGODB_DATABASE_NAME, username=ARANGODB_USERNAME, password=ARANGODB_PASSWORD
+database = ArangoDB(
+    getenv('ARANGODB_DATABASE_URL'),
+    getenv('ARANGODB_DATABASE_NAME'),
+    getenv('ARANGODB_USERNAME'),
+    getenv('ARANGODB_PASSWORD')
 )
--- database/model.py
+++ database/model.py
@@ -1,61 +1,60 @@
+import time
+import ujson
 from arango import ArangoClient
 from arango.database import StandardDatabase, AsyncDatabase
-# from arango.collection import StandardCollection
 from arango.job import AsyncJob
-import ujson
-import time
 
 
-client = ArangoClient(hosts='http://localhost:8529')
-db = client.db('genentechdb', username='root', password='pass')
+def receive_async_response(task: AsyncJob):
+    while task.status() != 'done':
+        time.sleep(0.1)
+    return [i for i in task.result()]
 
 
-class ArangoDB():
+class ArangoDB:
     client: ArangoClient
     instance: StandardDatabase
     async_instance: AsyncDatabase
-    tasks: dict
 
     def __init__(
         self,
-        hosts: str = "0.0.0.0:8529",
-        database: str = "genentechdb",
-        username: str = "",
-        password: str = "",
+        hosts: str = '0.0.0.0:8529',
+        database: str = 'genentechdb',
+        username: str = '',
+        password: str = '',
     ):
         self.client = ArangoClient(
-            hosts=hosts,
+            hosts,
             serializer=ujson.dumps,
             deserializer=ujson.loads,
         )
-        self.instance = self.client.db(database, username=username, password=password)
+        self.instance = self.client.db(database, username, password)
         self.async_instance = self.instance.begin_async_execution(return_result=True)
 
-    def receive_asynс_response(self, task: AsyncJob):
-        while task.status() != 'done':
-            time.sleep(0.1)
-        return [i for i in task.result()]
-
-    def Initialize(self, db=db):
+    def initialize(self):
         sys_db = self.client.db('_system', username='root', password='pass')
+
         if not sys_db.has_database('genentechdb'):
             sys_db.create_database('genentechdb')
+
         db = self.instance
+
         if not db.has_collection('users'):
             db.create_collection('users', edge=True)
+
         if not db.has_collection('groups'):
             db.create_collection('groups', edge=True)
 
     def insert(self, collection, data):
-        return db.insert_document(collection, data, True)
+        return self.instance.insert_document(collection, data, True)
 
-    def select(self, collection, filter, value):
-        task = self.async_instance.aql.execute('FOR doc IN ' + collection + ' ' + filter + ' RETURN doc ', bind_vars={'value': value})
-        emails = self.receive_asynс_response(task)
-        return emails
+    def select(self, collection, search='', **kwargs):
+        task = self.async_instance.aql.execute(
+            'FOR doc IN {} {} RETURN doc'.format(collection, search),
+            bind_vars={
+                **kwargs
+            }
+        )
+        return receive_async_response(task)
 
-    def selectUser(self, value):
-        task = self.async_instance.aql.execute('FOR doc IN users FILTER doc.email == @value LIMIT 1 RETURN  doc ',
-                                               bind_vars={'value': value}, count=True)
-        users = self.receive_asynс_response(task)
-        return users
+    # TODO delete and update
