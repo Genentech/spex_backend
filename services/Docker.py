6a3dff83b545cde2ea8cf075803d233c615680c2
Artem Zubkov <zubkov.artem@gene.com>
2021-06-11 13:02:57 +0700

    added route to stop, start containers, delete containers, start auto-remove container, get container log

--- services/Docker.py
+++ services/Docker.py
@@ -2,6 +2,7 @@ import docker
 import os
 client = docker.from_env()
 
+
 envfordocker = {
     'FLASK_ENV': 'production',
     'FLASK_DEBUG': True,
@@ -15,21 +16,41 @@ envfordocker = {
 }
 
 
-def listContainers():
-    service = Image()
+def list_containers(author=None, name='', **kwargs):
+    service = image()
     if service is None:
         return None
-    return client.containers.list(all=True, filters={'ancestor': service.id})
+    filters = {'ancestor': service.id}
+    if author is not None:
+        filters.update({'label': [f'id={author.get("id")}', f'login={author.get("login")}']})
+    if name is not None:
+        filters.update({'name': name})
+    if kwargs is not None:
+        filters.update(kwargs)
+    return client.containers.list(all=True, filters=filters)
 
 
-def Image():
+def prune_containers(**kwargs):
+    service = image()
+    filters = {}
+    if service is None:
+        return None
+    if kwargs is not None:
+        filters.update(kwargs)
+    return client.containers.prune(filters=filters)
+
+
+def image():
     images = client.images.list(filters={'reference': os.getenv('DOCKER_IMAGE')})
     service = images[0] if len(images) > 0 else None
     return service
 
 
-def mountAndStartContainer(author, path):
-    service = Image()
+def mount_and_start_container(author, path, name='', auto_remove=False):
+    service = image()
+    string = ''
+    if name != '':
+        string = '-'
     if service is None:
         return None
     container = client.containers.run(
@@ -38,8 +59,8 @@ def mountAndStartContainer(author, path):
                                       volumes={path: {'bind': '/DATA_STORAGE', 'mode': 'rw'}},
                                       labels=author,
                                       detach=True,
-                                      name=author.get('id'),
+                                      name=author.get('id') + string+''.join(filter(str.isalnum, name)),
                                       environment=envfordocker,
-                                      auto_remove=True
+                                      auto_remove=auto_remove
                                       )
     return container
