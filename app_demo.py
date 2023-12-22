ceed8f6af4a0f7ce91a34dedd905b97d1292cc68
Aleksandr Prilipko <zverozabr@yandex.ru>
2023-12-22 14:27:34 +0700

    added demo building

---
+++ app_demo.py
@@ -0,0 +1,31 @@
+from spex_common.config import load_config
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from routes import blueprint
+from flask_compress import Compress
+import logging
+
+config = load_config()
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger("werkzeug").setLevel(logging.WARNING)
+logging.getLogger("matplotlib.font_manager").setLevel(logging.WARNING)
+logging.getLogger("PIL.PngImagePlugin").setLevel(logging.WARNING)
+logging.getLogger("matplotlib.axes._base").setLevel(logging.WARNING)
+
+application = Flask(__name__)
+Compress(application)
+
+application.config.from_mapping(config)
+
+
+bcrypt = Bcrypt(application)
+jwt = JWTManager(application)
+CORS(application, supports_credentials=True)
+
+
+application.register_blueprint(blueprint)
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', debug=True)
