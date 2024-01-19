93f0f8b0d69e269acd1217186fe15be583773300
Aleksandr Prilipko <zverozabr@yandex.ru>
2024-01-19 16:36:36 +0700

    changed build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 docker build -f ./backend/Demo_Dockerfile -t spex.backend:latest .
+docker tag spex.backend:latest ghcr.io/genentech/spex.backend:latest
+docker push ghcr.io/genentech/spex.backend:latest
